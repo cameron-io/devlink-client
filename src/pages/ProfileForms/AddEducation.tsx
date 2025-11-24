0daf512a225847261bce355cb07b3ad9794e549a
Cameron Gallichan <camerongallichan@gmail.com>
2025-11-24 20:38:37 +0000

    improve education & experience form styles

--- src/pages/ProfileForms/AddEducation.tsx
+++ src/pages/ProfileForms/AddEducation.tsx
@@ -39,92 +39,108 @@ const AddEducation: FunctionComponent<Props> = ({ addEducation }) => {
                 <i className="fas fa-code-branch"></i> Add any school or
                 bootcamp you have attended
             </p>
-            <small>* = required field</small>
-            <form
-                className="form"
-                onSubmit={(e) => {
-                    e.preventDefault()
-                    addEducation(formData, navigate)
-                }}
-            >
-                <div className="form-group">
-                    <input
-                        type="text"
-                        placeholder="* School or Bootcamp"
-                        name="school"
-                        value={school}
-                        onChange={(e) => onChange(e)}
-                        required
-                    />
+            <div className='d-flex align-items-center py-4 bg-body-tertiary border rounded'>
+                <div className='w-100 m-auto mt-4' style={{maxWidth: "330px", padding: "1rem"}}>
+                    <h1 className="h3 mb-3 fw-normal">Education Details</h1>
+                    <small>* = required field</small>
+                    <form
+                        className="form"
+                        onSubmit={(e) => {
+                            e.preventDefault()
+                            addEducation(formData, navigate)
+                        }}
+                    >
+                        <div className="form-floating">
+                            <input
+                                className='form-control'
+                                type="text"
+                                placeholder="* School or Bootcamp"
+                                name="school"
+                                value={school}
+                                onChange={(e) => onChange(e)}
+                                required
+                            />
+                            <label htmlFor="floatingInput">* School or Bootcamp</label>
+                        </div>
+                        <div className="form-floating">
+                            <input
+                                className='form-control'
+                                type="text"
+                                placeholder="* Degree or Certificate"
+                                name="degree"
+                                value={degree}
+                                onChange={(e) => onChange(e)}
+                                required
+                            />
+                            <label htmlFor="floatingInput">* Degree or Certificate</label>
+                        </div>
+                        <div className="form-floating">
+                            <input
+                                className='form-control'
+                                type="text"
+                                placeholder="Field of Study"
+                                name="fieldofstudy"
+                                value={fieldofstudy}
+                                onChange={(e) => onChange(e)}
+                            />
+                            <label htmlFor="floatingInput">Field of Study</label>
+                        </div>
+                        <div className="form-floating my-3">
+                            <h4>From Date</h4>
+                            <input
+                                className='form-control'
+                                type="date"
+                                name="from"
+                                value={from}
+                                onChange={(e) => onChange(e)}
+                            />
+                        </div>
+                        <div className="form-check my-3">
+                            <p>
+                                <input
+                                    className='form-check-input'
+                                    type="checkbox"
+                                    name="current"
+                                    checked={current}
+                                    value={current.toString()}
+                                    onChange={(_e) => {
+                                        setFormData({ ...formData, current: !current })
+                                        toggleDisabled(!toDateDisabled)
+                                    }}
+                                />{' '}
+                                Current Education
+                            </p>
+                        </div>
+                        <div className="form-floating">
+                            <h4>To Date</h4>
+                            <input
+                                className='form-control'
+                                type="date"
+                                name="to"
+                                value={to}
+                                onChange={(e) => onChange(e)}
+                                disabled={toDateDisabled}
+                            />
+                        </div>
+                        <div className="form-floating">
+                            <textarea
+                                name="description"
+                                className='form-control'
+                                cols={30}
+                                rows={5}
+                                placeholder="Programme Description"
+                                value={description}
+                                onChange={(e) => onChange(e)}
+                            ></textarea>
+                            <label htmlFor="floatingInput">Description</label>
+                        </div>
+                        <input type="submit" className="btn btn-primary my-1" />
+                        <Link className="btn btn-light my-3 mx-2" to="/dashboard">
+                            Go Back
+                        </Link>
+                    </form>
                 </div>
-                <div className="form-group">
-                    <input
-                        type="text"
-                        placeholder="* Degree or Certificate"
-                        name="degree"
-                        value={degree}
-                        onChange={(e) => onChange(e)}
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <input
-                        type="text"
-                        placeholder="Field of Study"
-                        name="fieldofstudy"
-                        value={fieldofstudy}
-                        onChange={(e) => onChange(e)}
-                    />
-                </div>
-                <div className="form-group">
-                    <h4>From Date</h4>
-                    <input
-                        type="date"
-                        name="from"
-                        value={from}
-                        onChange={(e) => onChange(e)}
-                    />
-                </div>
-                <div className="form-group">
-                    <p>
-                        <input
-                            type="checkbox"
-                            name="current"
-                            checked={current}
-                            value={current.toString()}
-                            onChange={(_e) => {
-                                setFormData({ ...formData, current: !current })
-                                toggleDisabled(!toDateDisabled)
-                            }}
-                        />{' '}
-                        Current Education
-                    </p>
-                </div>
-                <div className="form-group">
-                    <h4>To Date</h4>
-                    <input
-                        type="date"
-                        name="to"
-                        value={to}
-                        onChange={(e) => onChange(e)}
-                        disabled={toDateDisabled}
-                    />
-                </div>
-                <div className="form-group">
-                    <textarea
-                        name="description"
-                        cols={30}
-                        rows={5}
-                        placeholder="Programme Description"
-                        value={description}
-                        onChange={(e) => onChange(e)}
-                    ></textarea>
-                </div>
-                <input type="submit" className="btn btn-primary my-1" />
-                <Link className="btn btn-light my-1" to="/dashboard">
-                    Go Back
-                </Link>
-            </form>
+            </div>
         </Fragment>
     )
 }
--- src/pages/ProfileForms/AddExperience.tsx
+++ src/pages/ProfileForms/AddExperience.tsx
@@ -39,92 +39,108 @@ const AddExperience: FunctionComponent<Props> = ({ addExperience }) => {
                 <i className="fas fa-code-branch"></i> Add any
                 developer/programming positions that you have had in the past
             </p>
-            <small>* = required field</small>
-            <form
-                className="form"
-                onSubmit={(e) => {
-                    e.preventDefault()
-                    addExperience(formData, navigate)
-                }}
-            >
-                <div className="form-group">
-                    <input
-                        type="text"
-                        placeholder="* Job Title"
-                        name="title"
-                        value={title}
-                        onChange={(e) => onChange(e)}
-                        required
-                    />
+            <div className='d-flex align-items-center py-4 bg-body-tertiary border rounded'>
+                <div className='w-100 m-auto mt-4' style={{maxWidth: "330px", padding: "1rem"}}>
+                    <h1 className="h3 mb-3 fw-normal">Work Experience Details</h1>
+                    <small>* = required field</small>
+                    <form
+                        className="form"
+                        onSubmit={(e) => {
+                            e.preventDefault()
+                            addExperience(formData, navigate)
+                        }}
+                    >
+                        <div className="form-floating">
+                            <input
+                                className='form-control'
+                                type="text"
+                                placeholder="* Job Title"
+                                name="title"
+                                value={title}
+                                onChange={(e) => onChange(e)}
+                                required
+                            />
+                            <label htmlFor="floatingInput">* Job Title</label>
+                        </div>
+                        <div className="form-floating">
+                            <input
+                                className='form-control'
+                                type="text"
+                                placeholder="* Company"
+                                name="company"
+                                value={company}
+                                onChange={(e) => onChange(e)}
+                                required
+                            />
+                            <label htmlFor="floatingInput">* Company</label>
+                        </div>
+                        <div className="form-floating">
+                            <input
+                                className='form-control'
+                                type="text"
+                                placeholder="Location"
+                                name="location"
+                                value={location}
+                                onChange={(e) => onChange(e)}
+                            />
+                            <label htmlFor="floatingInput">Location</label>
+                        </div>
+                        <div className="form-floating my-3">
+                            <h4>From Date</h4>
+                            <input
+                                className='form-control'
+                                type="date"
+                                name="from"
+                                value={from}
+                                onChange={(e) => onChange(e)}
+                            />
+                        </div>
+                        <div className="form-check my-3">
+                            <p>
+                                <input
+                                    className='form-check-input'
+                                    type="checkbox"
+                                    name="current"
+                                    checked={current}
+                                    value={current.toString()}
+                                    onChange={(_e) => {
+                                        setFormData({ ...formData, current: !current })
+                                        toggleDisabled(!toDateDisabled)
+                                    }}
+                                />{' '}
+                                Current Job
+                            </p>
+                        </div>
+                        <div className="form-floating">
+                            <h4>To Date</h4>
+                            <input
+                                className='form-control'
+                                type="date"
+                                name="to"
+                                value={to}
+                                onChange={(e) => onChange(e)}
+                                disabled={toDateDisabled}
+                            />
+                        </div>
+                        <div className="form-floating">
+                            <textarea
+                                className='form-control'
+                                name="description"
+                                cols={30}
+                                rows={5}
+                                placeholder="Job Description"
+                                value={description}
+                                onChange={(e) => onChange(e)}
+                            ></textarea>
+                            <label htmlFor="floatingInput">Job Description</label>
+                        </div>
+                        <input type="submit" className="btn btn-primary my-1" />
+                        <Link className="btn btn-light my-3 mx-2" to="/dashboard">
+                            Go Back
+                        </Link>
+                    </form>
                 </div>
-                <div className="form-group">
-                    <input
-                        type="text"
-                        placeholder="* Company"
-                        name="company"
-                        value={company}
-                        onChange={(e) => onChange(e)}
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <input
-                        type="text"
-                        placeholder="Location"
-                        name="location"
-                        value={location}
-                        onChange={(e) => onChange(e)}
-                    />
-                </div>
-                <div className="form-group">
-                    <h4>From Date</h4>
-                    <input
-                        type="date"
-                        name="from"
-                        value={from}
-                        onChange={(e) => onChange(e)}
-                    />
-                </div>
-                <div className="form-group">
-                    <p>
-                        <input
-                            type="checkbox"
-                            name="current"
-                            checked={current}
-                            value={current.toString()}
-                            onChange={(_e) => {
-                                setFormData({ ...formData, current: !current })
-                                toggleDisabled(!toDateDisabled)
-                            }}
-                        />{' '}
-                        Current Job
-                    </p>
-                </div>
-                <div className="form-group">
-                    <h4>To Date</h4>
-                    <input
-                        type="date"
-                        name="to"
-                        value={to}
-                        onChange={(e) => onChange(e)}
-                        disabled={toDateDisabled}
-                    />
-                </div>
-                <div className="form-group">
-                    <textarea
-                        name="description"
-                        cols={30}
-                        rows={5}
-                        placeholder="Job Description"
-                        value={description}
-                        onChange={(e) => onChange(e)}
-                    ></textarea>
-                </div>
-                <input type="submit" className="btn btn-primary my-1" />
-                <Link className="btn btn-light my-1" to="/dashboard">
-                    Go Back
-                </Link>
-            </form>
+            </div>
         </Fragment>
     )
 }
