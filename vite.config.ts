3f432789713e51f307d105e413f199517aac4724
Cameron Gallichan <camerongallichan@gmail.com>
2025-11-30 22:06:53 +0000

    make addr accessible to local network

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,11 @@ export default defineConfig(() => {
         },
         plugins: [react()],
         server: {
+            host: '0.0.0.0',
             port: 3000,
             proxy: {
                 '/api': {
-                    target: 'http://localhost:5000',
+                    target: 'http://0.0.0.0:5000',
                     changeOrigin: true,
                     secure: false,
                     ws: true,
