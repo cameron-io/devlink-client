ba3c8806a155c33f8a407592da546f5499e44344
Cameron Gallichan <camerongallichan@gmail.com>
2025-12-16 14:18:56 +0000

    update server host for local dev

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(() => {
             port: 3000,
             proxy: {
                 '/api': {
-                    target: 'http://0.0.0.0:5000',
+                    target: 'http://localhost:5000',
                     changeOrigin: true,
                     secure: false,
                     ws: true,
